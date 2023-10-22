7e4963a693b488802e8e5d6e8c4d470ffdca4e2c
RazvanChereghi <razvanchereghi@yahoo.com>
2023-10-22 17:48:19 +0300

    Add project files.

---
+++ Chereghi_Razvan_Lab2/Data/Chereghi_Razvan_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chereghi_Razvan_Lab2.Models;
+
+namespace Chereghi_Razvan_Lab2.Data
+{
+    public class Chereghi_Razvan_Lab2Context : DbContext
+    {
+        public Chereghi_Razvan_Lab2Context (DbContextOptions<Chereghi_Razvan_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Chereghi_Razvan_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Chereghi_Razvan_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
